06e3511f154efac815f09baad491202d453c5a57
phkolarov <ph.kolarov@gmail.com>
2015-10-04 10:20:57 +0300

    Lets goo

---
+++ app.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Phill
+ * Date: 10/4/2015
+ * Time: 10:07 AM
+ */
+class app
+{
+
+}
---
+++ areas/administrator/controllers/AdministratorController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Phill
+ * Date: 10/4/2015
+ * Time: 9:35 AM
+ */
